44fa0c86456b9bff1eb22260e3c918dac665848c
Hinata1411 <85373964+Hinata1411@users.noreply.github.com>
2021-10-15 21:32:16 -0600

    Add files via upload

---
+++ Inserción_directa/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de números no ordenados");
+            int[] lista = {38, 23, 54, 93, 12, 4, 21, 3, 95, 14, 53, 89, 78, 67};
+            mostrar(lista);
+            Console.WriteLine("");
+            OrdenacionDirecta(lista);
+            Console.ReadLine();
+            Console.WriteLine("Lista de números ordenados por medio del algoritmo de inserción directa");
+            mostrar(lista);
+            Console.ReadLine();
+        }
+
+        public static void OrdenacionDirecta(int[] lista)
+        {
+            for (int i = 0; i < lista.Length; i++)
+            {
+                int varaux = lista[i];
+                int j = i - 1;
+                while (j >= 0 && lista[j] > varaux)
+                {
+                    lista[j + 1] = lista[j];
+                    j--;
+                }lista[j + 1] = varaux;
+            }
+        }
+        private static void mostrar(int[] lista)
+        {
+            foreach (int i in lista)
+                Console.Write(" " + i.ToString());
+        }
+    }
+}
